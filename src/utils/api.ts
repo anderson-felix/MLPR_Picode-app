b54aa9fe790da53824ceb84d5ef89e728af2262b
Anderson Felix <63384863+anderson-felix@users.noreply.github.com>
2021-02-24 19:17:33 -0300

    Create api.ts

---
+++ src/utils/api.ts
@@ -0,0 +1,11 @@
+import Axios from "axios";
+
+export const api = Axios.create({
+  baseURL: "http://localhost:3333/",
+  withCredentials: true,
+});
+
+api.interceptors.response.use(
+  (resp) => resp.data,
+  (error) => Promise.reject(error)
+);
